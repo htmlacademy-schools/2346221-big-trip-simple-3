25311712276817bb794ea8c20a6f32d9073b9934
Gloff228 <romo4ka101@gmail.com>
2023-05-25 19:15:48 +0300

    filters

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ const tripEventsSection = document.querySelector('.trip-events');
 
 const tripEventsModel = new TripEventsModel();
 const tripPresenter = new TripPresenter();
-
-render(new EventFiltersView(), tripFiltersSection);
 tripPresenter.init(tripEventsSection, tripEventsModel);
+
+const eventFilters = new EventFiltersView();
+
+const printSelectedFilter = () => console.log(eventFilters.selectedFilter);
+eventFilters.setFilterChangeListener(printSelectedFilter);
+render(eventFilters, tripFiltersSection);
+
+// надо добавить добавление формы при нажатие на кнопку
+
